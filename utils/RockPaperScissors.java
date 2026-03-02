833f45d3f52a6e318ea3bcd1f3ac6fa197cf562d
ilmaritak <takkunen.ilmari@gmail.com>
2026-03-02 19:05:03 +0200

    feat: add startGame method

--- utils/RockPaperScissors.java
+++ utils/RockPaperScissors.java
@@ -4,14 +4,20 @@ import java.io.Console;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RockPaperScissors {
-    public static void playRockPaperScissors() {
+    public void playRockPaperScissors() {
         int rockIndex = 0;
         int paperIndex = 1;
         int scissorIndex = 2;
+
+        int pcIndex = getRandomIndex(0, 2);
 
     }
 
     public int getRandomIndex(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    public void startGame() {
+        System.out.println("Welcome to the game of rock paper sciccors!");
     }
 }

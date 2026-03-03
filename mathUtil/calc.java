c6981c037f43a63f8cc961b3851ca9e307a0d1ff
Kimmo <niko.karhu02@gmail.com>
2026-03-03 09:19:51 +0200

    fix(calc): trim user input values

--- mathUtil/calc.java
+++ mathUtil/calc.java
@@ -9,11 +9,11 @@ public class calc {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Give number 1:");
-        String num1 = scan.nextLine();
+        String num1 = scan.nextLine().trim();
         System.out.println("Give operator (+, -, *, /):");
-        String op = scan.nextLine();
+        String op = scan.nextLine().trim();
         System.out.println("Give number 2:");
-        String num2 = scan.nextLine();
+        String num2 = scan.nextLine().trim();
         Float tulos = 0.0f;
         if (op.equals("+")) {
             tulos = Float.parseFloat(num1) + Float.parseFloat(num2);

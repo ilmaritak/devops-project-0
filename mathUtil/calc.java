e32bc79bb6c68ba103db730404049b38147e39dc
Kimmo <niko.karhu02@gmail.com>
2026-03-03 09:09:46 +0200

    feat(calc): prompt for first number

--- mathUtil/calc.java
+++ mathUtil/calc.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class calc {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Give number 1:");
+        String num1 = scan.nextLine();
     }
 }
